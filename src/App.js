152d866e99b89a83810a54df91c926c65a6fe2cc
V9CodeStack <v9codestack@gmail.com>
2023-04-04 10:55:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// FIX1: To use Switch component, it should be imported
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import TeamMatches from "./components/TeamMatches";
+
+import "./App.css";
+
+const App = () => (
+  // FIX2: Routes should be wrapped with Switch component from react-router-dom
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/team-matches/:id" component={TeamMatches} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
